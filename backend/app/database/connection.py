e1915a2d769ba7c393d983b16cc71a908dd8e59e
Kamal-Saltani-Datascience <kamalsaltani88@gmail.com>
2026-08-24 21:31:01 -0400

    Secure database configuration

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -1,15 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from dotenv import load_dotenv
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://kamalsaltani:REMOVED_SECRET@localhost:5432/stockdb"
-)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
+
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 
 # -----------------------------------
 # FASTAPI dependency (ONLY FOR API)
